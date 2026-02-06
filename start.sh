9309ab3f27abfc0e73bf235085635778a8ed358a
rikmortin553-stack <rikmortin553@gmail.com>
2026-02-06 18:50:10 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 set +e 
 
 echo "----------------------------------------------------------------"
-echo "🚀 ЗАПУСК RTX 5090 (SAFE MODE)"
+echo "🚀 ЗАПУСК RTX 5090 (NO ACCELERATORS MODE)"
 echo "----------------------------------------------------------------"
 
 if [ ! -f "/workspace/ComfyUI/main.py" ]; then
@@ -12,7 +12,7 @@ fi
 echo "🔍 Проверяю GPU..."
 while ! nvidia-smi > /dev/null 2>&1; do sleep 2; done
 
-# --- ЗАГРУЗКА ФАЙЛОВ ---
+# --- ЗАГРУЗКА ---
 BASE_DIR="/workspace/ComfyUI/models"
 download_big() {
     mkdir -p "$1"
@@ -25,7 +25,6 @@ download_safe() {
 
 download_big "$BASE_DIR/diffusion_models" "Wan21_SteadyDancer_fp8_e4m3fn_scaled_KJ.safetensors" "https://huggingface.co/Kijai/WanVideo_comfy_fp8_scaled/resolve/main/SteadyDancer/Wan21_SteadyDancer_fp8_e4m3fn_scaled_KJ.safetensors"
 download_big "$BASE_DIR/loras" "lightx2v_I2V_14B_480p_cfg_step_distill_rank64_bf16.safetensors" "https://huggingface.co/dci05049/wan-animate/resolve/main/lightx2v_I2V_14B_480p_cfg_step_distill_rank64_bf16.safetensors"
-
 download_safe "$BASE_DIR/clip_vision" "clip_vision_h.safetensors" "https://huggingface.co/Comfy-Org/Wan_2.1_ComfyUI_repackaged/resolve/main/split_files/clip_vision/clip_vision_h.safetensors"
 download_safe "$BASE_DIR/vae" "Wan2_1_VAE_bf16.safetensors" "https://huggingface.co/Kijai/WanVideo_comfy/resolve/main/Wan2_1_VAE_bf16.safetensors"
 download_safe "$BASE_DIR/text_encoders" "umt5-xxl-enc-bf16.safetensors" "https://huggingface.co/Kijai/WanVideo_comfy/resolve/main/umt5-xxl-enc-bf16.safetensors"
@@ -41,5 +40,7 @@ echo "🏁 ЗАПУСК..."
 jupyter lab --ip=0.0.0.0 --port=8888 --no-browser --allow-root --NotebookApp.token='' --NotebookApp.password='' > /dev/null 2>&1 &
 
 cd /workspace/ComfyUI
-# ВАЖНО: Добавлен флаг --disable-xformers
+# ВАЖНО: 
+# --disable-xformers: запрещаем искать xformers
+# --disable-cuda-malloc: иногда помогает при ошибках памяти на новых картах
 python -u main.py --listen 0.0.0.0 --port 3001 --highvram --disable-xformers
